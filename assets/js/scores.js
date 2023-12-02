0d5c2621845275ebb94cad1c7e969b4ffe4b88bf
Matt Lightfoot <m-lightfoot@hotmail.com>
2023-12-02 12:12:37 +0000

    Added clear button which clear highscore page and local storage when clicked

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,12 +1,18 @@
+// query selectors
 const highscoresID = document.querySelector("#highscores");
+const clearButton = document.querySelector("#clear");
 
+// creates variable that holds all current local storage scores
 const highscoresFromStorage = JSON.parse(localStorage.getItem("highscores"));
 
-console.log(highscoresFromStorage);
-
+// for loop to add new scores to the high score table
 for (let i = 0; i < highscoresFromStorage.length; i++) {
     const score = document.createElement("li");
     score.textContent = highscoresFromStorage[i].initial + " - " + highscoresFromStorage[i].score;
     highscoresID.appendChild(score);
 }
 
+clearButton.addEventListener("click", function() {
+    localStorage.clear();
+    location.reload();
+});
